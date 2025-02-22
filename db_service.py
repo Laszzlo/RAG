9262f89975e49313d76051f91a4fc32251e093d6
Laszlo Imanuel <laszlo.imanuel@campus-tu.berlin.de>
2025-02-23 00:50:10 +0100

    Update...

--- db_service.py
+++ db_service.py
@@ -3,6 +3,7 @@ from pymilvus import connections, db
 from dotenv import load_dotenv
 
 load_dotenv()
+
 def create_connection(db_name):
     conn = connections.connect(
     host=os.getenv("host"),
@@ -14,13 +15,5 @@ def create_connection(db_name):
 def create_db(db_name):
     create_connection(db_name)
     database = db.create_database(db_name)
-
-def connect_db(db_name):
-    conn = connections.connect(
-    host="127.0.0.1",
-    port="19530",
-    db_name=db_name
-)
-    
-connect_db("rag_db")
+    return database
     
